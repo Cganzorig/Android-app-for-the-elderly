ca920c6650813f76dc329c725a61325d5e1b8dd0
user <user@dyn3187-85.wlan.ic.ac.uk>
2019-02-02 15:02:32 +0000

    Bare bones launcher and app drawer

---
+++ Launcher/app/src/main/java/com/launcher/ava/elderlylauncher/AppDrawer.java
@@ -0,0 +1,13 @@
+package com.launcher.ava.elderlylauncher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppDrawer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_drawer);
+    }
+}
---
+++ Launcher/app/src/main/java/com/launcher/ava/elderlylauncher/AppInfo.java
@@ -0,0 +1,10 @@
+package com.launcher.ava.elderlylauncher;
+
+import android.graphics.drawable.Drawable;
+
+public class AppInfo {
+    public CharSequence label;
+    public CharSequence packageName;
+    public Drawable icon;
+}
+
