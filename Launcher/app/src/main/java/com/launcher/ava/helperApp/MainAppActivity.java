adf7dc3f5777c0876d35eb09ef2c2c39d84d3bb6
user <user@users-MacBook-Pro.local>
2019-02-02 22:56:40 +0000

    Created a simple helper app, and seperated helper app and launcher into different packages. Adding new folder structure.

---
+++ Launcher/app/src/main/java/com/launcher/ava/helperApp/MainAppActivity.java
@@ -0,0 +1,15 @@
+package com.launcher.ava.helperApp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.launcher.ava.elderlylauncher.R;
+
+public class MainAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_app);
+    }
+}
