90b4516f6556eacbdf72e450fb83aea8c29cad4a
user <user@dyn3187-85.wlan.ic.ac.uk>
2019-02-02 17:48:49 +0000

    Deleted old files

--- crap/app/src/main/java/com/example/launcher_find_app/AppInfo.java
+++
@@ -1,9 +0,0 @@
-package com.example.launcher_find_app;
-
-import android.graphics.drawable.Drawable;
-
-public class AppInfo {
-    public CharSequence label;
-    public CharSequence packageName;
-    public Drawable icon;
-}
