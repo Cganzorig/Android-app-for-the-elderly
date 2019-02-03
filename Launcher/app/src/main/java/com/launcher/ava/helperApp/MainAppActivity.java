219887b21e3a57f55a68bd278b01979c8a41f151
user <user@users-MacBook-Pro.local>
2019-02-03 00:03:26 +0000

    Added grey background to helper app; added some placeholder text and buttons to helper app

--- Launcher/app/src/main/java/com/launcher/ava/helperApp/MainAppActivity.java
+++ Launcher/app/src/main/java/com/launcher/ava/helperApp/MainAppActivity.java
@@ -1,5 +1,6 @@
 package com.launcher.ava.helperApp;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +12,7 @@ public class MainAppActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_app);
+        // Change the background of the app drawer to white
+        getWindow().getDecorView().setBackgroundColor(Color.GRAY);
     }
 }
