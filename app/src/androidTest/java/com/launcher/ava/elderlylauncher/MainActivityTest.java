66325dc86c102d44768e42277e858ba6e0cc7675
Vesko Karadotchev <vk2018@ic.ac.uk>
2019-02-23 11:43:10 +0000

    Added a UI test to see if pressing the HelperApp button works

--- app/src/androidTest/java/com/launcher/ava/elderlylauncher/MainActivityTest.java
+++ app/src/androidTest/java/com/launcher/ava/elderlylauncher/MainActivityTest.java
@@ -1,12 +1,19 @@
 package com.launcher.ava.elderlylauncher;
 
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.mock;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.support.test.filters.SmallTest;
 import android.support.test.rule.ActivityTestRule;
 import android.view.View;
 import com.launcher.ava.frequentlyUsedAppsScreen.AppFrequencyList;
+import com.launcher.ava.helperApp.MainAppActivity;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -23,6 +30,8 @@ public class MainActivityTest {
 
   AppFrequencyList mockedAppFrequencyList = mock(AppFrequencyList.class);
 
+  Instrumentation.ActivityMonitor monitorHelperApp = getInstrumentation()
+      .addMonitor(MainAppActivity.class.getName(), null, false);
 
   @Before
   public void createMainActivity() {
@@ -47,6 +56,19 @@ public class MainActivityTest {
 
   @Test
   public void clickLaunchHelperAppOpensActivityMainApp() {
+
+    // checks click is working
+    assertNotNull(mainActivity.findViewById(R.id.helperApp));
+
+    onView(withId(R.id.helperApp)).perform(click());
+
+    Activity mainAppActivity = getInstrumentation()
+        .waitForMonitorWithTimeout(monitorHelperApp, 5000);
+
+    assertNotNull(mainAppActivity);
+    mainAppActivity.finish();
+
+
   }
 
   @Test
