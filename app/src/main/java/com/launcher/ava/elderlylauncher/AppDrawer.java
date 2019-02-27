b3643f390c153adf1210b1545a3cae6ef3823773
Vesko Karadotchev <vk2018@ic.ac.uk>
2019-02-27 18:45:55 +0000

    Wallpaper blur now cannot request premissions properly. Removed form both AppDrawer and FreqApps until we figure out how to grant permissions

--- app/src/main/java/com/launcher/ava/elderlylauncher/AppDrawer.java
+++ app/src/main/java/com/launcher/ava/elderlylauncher/AppDrawer.java
@@ -1,16 +1,9 @@
 package com.launcher.ava.elderlylauncher;
 
-import android.app.WallpaperManager;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-
-import com.launcher.ava.utilities.BlurBuilder;
 import com.launcher.ava.utilities.RemoveStatusBar;
 
 public class AppDrawer extends AppCompatActivity {
@@ -22,16 +15,16 @@ public class AppDrawer extends AppCompatActivity {
         RemoveStatusBar.remove(this);
         setContentView(R.layout.activity_app_drawer);
 
-        final WallpaperManager wallpaperManager = WallpaperManager.getInstance(this);
-        final Drawable wallpaperDrawable = wallpaperManager.getDrawable();
-
-
-        ConstraintLayout mContainerView = findViewById(R.id.appDrawerContainer);
-        Bitmap originalBitmap = BlurBuilder.drawableToBitmap(wallpaperDrawable);
-
-        Bitmap blurredBitmap = BlurBuilder.blur( this, originalBitmap );
-
-        mContainerView.setBackground(new BitmapDrawable(getResources(), blurredBitmap));
+//        final WallpaperManager wallpaperManager = WallpaperManager.getInstance(this);
+//        final Drawable wallpaperDrawable = wallpaperManager.getDrawable();
+//
+//
+//        ConstraintLayout mContainerView = findViewById(R.id.appDrawerContainer);
+//        Bitmap originalBitmap = BlurBuilder.drawableToBitmap(wallpaperDrawable);
+//
+//        Bitmap blurredBitmap = BlurBuilder.blur( this, originalBitmap );
+//
+//        mContainerView.setBackground(new BitmapDrawable(getResources(), blurredBitmap));
 
 
         RecyclerView recyclerView = findViewById(R.id.RView);
--- app/src/main/java/com/launcher/ava/frequentlyUsedAppsScreen/FrequentlyUsedAppsActivity.java
+++ app/src/main/java/com/launcher/ava/frequentlyUsedAppsScreen/FrequentlyUsedAppsActivity.java
@@ -1,20 +1,13 @@
 package com.launcher.ava.frequentlyUsedAppsScreen;
 
-import android.app.WallpaperManager;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.support.constraint.ConstraintLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import com.launcher.ava.elderlylauncher.R;
 import com.launcher.ava.utilities.AppFrequencyList;
-import com.launcher.ava.utilities.BlurBuilder;
 import com.launcher.ava.utilities.RemoveStatusBar;
 
 public class FrequentlyUsedAppsActivity extends AppCompatActivity {
@@ -29,16 +22,16 @@ public class FrequentlyUsedAppsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_frequently_used_apps);
 
 
-        final WallpaperManager wallpaperManager = WallpaperManager.getInstance(this);
-        final Drawable wallpaperDrawable = wallpaperManager.getDrawable();
+//        final WallpaperManager wallpaperManager = WallpaperManager.getInstance(this);
+//        final Drawable wallpaperDrawable = wallpaperManager.getDrawable();
+//
+//
+//        ConstraintLayout mContainerView = (ConstraintLayout) findViewById(R.id.container);
+//        Bitmap originalBitmap = BlurBuilder.drawableToBitmap(wallpaperDrawable);
+//
+//        Bitmap blurredBitmap = BlurBuilder.blur( this, originalBitmap );
 
-
-        ConstraintLayout mContainerView = (ConstraintLayout) findViewById(R.id.container);
-        Bitmap originalBitmap = BlurBuilder.drawableToBitmap(wallpaperDrawable);
-
-        Bitmap blurredBitmap = BlurBuilder.blur( this, originalBitmap );
-
-        mContainerView.setBackground(new BitmapDrawable(getResources(), blurredBitmap));
+//        mContainerView.setBackground(new BitmapDrawable(getResources(), blurredBitmap));
 
         final TextView textApp1 = (TextView) findViewById(R.id.textApp1);
         textApp1.setText(AppFrequencyList.getInstance().getHit(0).label);
