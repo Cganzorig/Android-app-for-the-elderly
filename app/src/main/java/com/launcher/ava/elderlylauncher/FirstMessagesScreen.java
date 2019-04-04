cc42a87a08465785149aebc3c3053341bb3a9be0
Vesko Karadotchev <vk2018@ic.ac.uk>
2019-04-04 20:03:13 +0100

    Repeated George reg expression fix for First Phone screen to First Message Screen

--- app/src/main/java/com/launcher/ava/elderlylauncher/FirstMessagesScreen.java
+++ app/src/main/java/com/launcher/ava/elderlylauncher/FirstMessagesScreen.java
@@ -387,8 +387,8 @@ public class FirstMessagesScreen extends AppCompatActivity {
     while (cursor.moveToNext()) {
       String phoneNumber = cursor.getString(cursor.getColumnIndex(Phone.NUMBER));
       if (phoneNumber != null) {
-        phoneNumber = phoneNumber.replace(" ", "");
-        target = target.replace(" ", "");
+        phoneNumber = phoneNumber.replaceAll("\\s+","");
+        target = target.replaceAll("\\s+","");
       }
       if (phoneNumber != null && phoneNumber.contains(target.substring(2))) {
         String _id = cursor.getString(cursor.getColumnIndex(ContactsContract.Data._ID));
