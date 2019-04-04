e5b1ad93b9b6169c1bc24a82967804ef5fdf31c6
gh1718 <gh1718@ic.ac.uk>
2019-04-04 10:45:06 +0100

    George: changed sorting function to be case insensitive

--- app/src/main/java/com/launcher/ava/utilities/AppInfo.java
+++ app/src/main/java/com/launcher/ava/utilities/AppInfo.java
@@ -14,9 +14,9 @@ public class AppInfo implements Comparable<AppInfo> {
       if (i >= o.label.length()) {
         return 1;
       }
-      if (this.label.charAt(i) > o.label.charAt(i)) {
+      if (Character.toLowerCase(this.label.charAt(i)) > Character.toLowerCase(o.label.charAt(i))) {
         return 1;
-      } else if (this.label.charAt(i) < o.label.charAt(i)) {
+      } else if (Character.toLowerCase(this.label.charAt(i)) < Character.toLowerCase(o.label.charAt(i))) {
         return -1;
       }
     }
