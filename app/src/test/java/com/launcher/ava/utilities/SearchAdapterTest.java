5db7bfa2e68c74e7450ae9b8696a162cbb1239f6
Charles <cpf18@ic.ac.uk>
2019-05-12 02:22:37 +0100

    TESTS 52% Classes, 30% lines

--- app/src/test/java/com/launcher/ava/utilities/SearchAdapterTest.java
+++ app/src/test/java/com/launcher/ava/utilities/SearchAdapterTest.java
@@ -1,20 +1,57 @@
 package com.launcher.ava.utilities;
 
-import org.junit.Test;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.launcher.ava.elderlylauncher.R;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.launcher.ava.elderlylauncher.R.layout.activity_app_drawer;
 import static org.junit.Assert.*;
 
 public class SearchAdapterTest {
 
-  @Test
-  public void getCount() {
+  private SearchAdapter searchAdapter;
+  private List<String> dataList;
+
+  @Mock
+  private ViewGroup viewGroup;
+  private Context mContext;
+  private int searchResultItemLayout;
+
+  @Before
+  public void init(){
+    dataList = new ArrayList<String>();
+    dataList.add("A");
+    dataList.add("B");
+    dataList.add("C");
+    dataList.add("a");
+
+    searchAdapter = new SearchAdapter(mContext, searchResultItemLayout, dataList);
+
+
   }
 
   @Test
-  public void getItem() {
+  public void getCountTest() {
+    assertEquals(4, searchAdapter.getCount());
   }
 
   @Test
-  public void getView() {
+  public void getItemTest() {
+    assertEquals("a", searchAdapter.getItem(3));
+  }
+
+  @Test
+  public void getViewTest() {
+
   }
 }
